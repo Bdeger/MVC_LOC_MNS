5c3262626be2140ad1ad1c10fdd214956447fd41
Bdeger <degerbanu2@gmail.com>
2026-05-28 11:07:41 +0200

    feat: header page acceuil dashboard

---
+++ app/Views/templates/admin/dashboard.php
@@ -0,0 +1,19 @@
+<section class="acceuil_dashboard">
+    <div class="header">
+        <div>
+            <h1>LocMns</h1>
+            <h2>Dashboard Admin</h2>
+        </div>
+        <div>
+            <a href="">Admin</a>
+        </div>
+    </div>
+    <div class="navbar">
+        <ul>
+            <li><a href=""></a>Dashboard</li>
+            <li><a href="">Emprunts</a></li>
+            <li><a href="">Matériel</a></li>
+            <li><a href="">Membres</a></li>
+        </ul>
+    </div>
+</section>
